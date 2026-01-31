7088ecbce3af42b2d1d8bedc97bcf5cfe1a55dda
ii19 <ii19@users.noreply.github.com>
2026-01-31 14:48:37 +0100

    format

--- main.cpp
+++ main.cpp
@@ -1,31 +1,32 @@
 #include "isa.hpp"
 
-auto rfile(auto name)
-{
+auto rfile ( auto name ) {
     std::ifstream file{name, std::ifstream::in};
 
-    file.seekg(0, std::ios::end);
+    file.seekg ( 0, std::ios::end );
     auto size = file.tellg();
-    file.seekg(0, std::ios::beg);
+    file.seekg ( 0, std::ios::beg );
 
-    std::vector<char> buffer(size);
-    file.read(buffer.data(), size);
+    std::vector<char> buffer ( size );
+    file.read ( buffer.data(), size );
     file.close();
 
     return buffer;
 }
 
-int main()
-{
+
+
+int main() {
     std::string path;
     std::cin >> path;
-    auto input = rfile(path.c_str());
+    auto input = rfile ( path );
     auto begin = input.begin();
     auto end = input.end();
     std::variant<instr_r, instr_i, instr_s> v;
-    for (auto it = 0; begin != end; ++it) {
-        phrase_parse(begin, end, parser, x3::ascii::space, v);
-        std::visit(visitor, isa::f[std::visit(instr_binding_V<0>, v)], v);
+    for ( auto it = 0; begin != end; ++it ) {
+        phrase_parse ( begin, end, parser, x3::ascii::space, v );
+        std::visit ( encode, isa::f[std::visit ( instr_binding_V<0>, v )], v );
         std::cout << isa::machine[it] << "\n";
     }
 }
+
